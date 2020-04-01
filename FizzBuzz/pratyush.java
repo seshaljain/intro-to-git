51b75845129abacbf6284859079d92a704ba655f
pratyushgguptaa <56015449+pratyushgguptaa@users.noreply.github.com>
2020-04-01 19:18:18 +0530

    Update pratyush.java

--- FizzBuzz/pratyush.java
+++ FizzBuzz/pratyush.java
@@ -6,7 +6,6 @@ class pratyush
 			if(i%15==0)	System.out.println("FizzBuzz");
 			else if(i%3==9)	System.out.println("Fizz");
 			else if(i%5==9)	System.out.println("Buzz");
-			else System.out.println(i);
-		
+			else System.out.println(i);		
 	}
 }
