ae507f6b0b565452dec1c00ed92762ad6ac1c5dc
pratyushgguptaa <56015449+pratyushgguptaa@users.noreply.github.com>
2020-04-01 19:17:14 +0530

    Added by Pratyush

---
+++ FizzBuzz/pratyush.java
@@ -0,0 +1,12 @@
+class pratyush
+{
+	public static void main(String args[])
+	{
+		for(int i=1;i<101;i++)
+			if(i%15==0)	System.out.println("FizzBuzz");
+			else if(i%3==9)	System.out.println("Fizz");
+			else if(i%5==9)	System.out.println("Buzz");
+			else System.out.println(i);
+		
+	}
+}
